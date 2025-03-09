d8dec8f75255b21ac3d9d17caacd08a394a9ffc1
somi9954 <jolh999@gmail.com>
2025-03-10 00:18:41 +0900

    day02

---
+++ day02/review/index.js
@@ -0,0 +1,8 @@
+const todo = window.prompt("주말에 뭐해요?");
+const food = window.prompt("뭐 먹고 싶어요?");
+
+window.console.log(`주말에 ${todo}일과 ${food} 음식을 드셔보세요!`);
+
+const age = window.prompt("몇살?");
+const number_age = Number(age);
+window.console.log(`${number_age}나이는 ${2026 - number_age}년생 이시군요!`);
